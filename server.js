199f494e27586f2ab2177c0652b5a105931218ed
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-01-02 23:17:48 +0100

    changed cors origin for live url

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://dreyxinx.vercel.app/",
   credentials: true,
 }))
 const PORT = process.env.PORT || 2007;
